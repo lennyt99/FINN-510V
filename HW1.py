28162c5c061ce2d5622406aa2f3cba7616f00d73
lennyt99 <100719908+lennyt99@users.noreply.github.com>
2022-03-01 14:31:47 -0600

    Add files via upload

--- HW1.py
+++ HW1.py
@@ -10,14 +10,10 @@ HW1DATA = pd.read_csv('C:\\Users\\lenny\\Fin510\\HW1.csv')
 HW1DATA.head()
 HW1DATA ["Date"] = pd.to_datetime(HW1DATA["Date"])
 
-#Now we merge using a list that we have created above
 
 dfl=[HW1DATA]
 df = df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['Date'],
                                             how='outer'), dfl)
-
-#Since we have what we need to graph, lets plot the returns calculated in xle
-#Stock column = Price Today (Clse Day 2) / Price Yesterday (Clse D1) - 1
 
 plt.plot(df["Date"],df["TSLA"], label="TSLA")
 plt.plot(df["Date"],df["MSFT"], label="MSFT")
@@ -27,7 +23,7 @@ plt.plot(df["Date"],df["MSFTNQ"], label="MSFTNQ")
 plt.plot(df["Date"],df["TSLANQ"], label="TSLANQ")
 
 
-#This is pretty much the design of the plot
+
 plt.tight_layout()
 plt.title("YF & BB Data BABA, SOFI, & SPY Stock Returns", size = 20)
 plt.legend() 
